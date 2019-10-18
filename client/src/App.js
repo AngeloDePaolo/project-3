a93571e426c6c2cdf7a3391b0bb213a241a091ff
gundrm <rachelm.gundy@gmail.com>
2019-10-17 21:04:28 -0700

    react component foundation

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import AppNavbar from './components/AppNavbar';
+import Modal from './components/Modal';
+import { Container } from 'reactstrap';
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+
+        <AppNavbar />
+        <Container>
+
+        {/* <Modal /> */}
+        {/* <HomePage /> */}
+
+
+        </Container>
+
+    </div>
     );
   }
 }

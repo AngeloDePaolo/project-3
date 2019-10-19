472a83c5371c57328834423a59685ae4b503bfec
Angelo DePaolo <adepaolo@gmail.com>
2019-10-19 13:51:51 -0700

    adding components for betbutton and modal

---
+++ client/src/components/Modal.js
@@ -0,0 +1,39 @@
+import React from "react";
+// import 
+
+class Modal extends React.Component {
+    componentDidMount() {
+    }
+
+    dismissModal = () => {
+        this.props.toggle()
+    }
+
+    render() {
+        return (
+            <div
+                onClick={this.dismissModal}
+                className={`modal fade BettingModal ${this.props.showModal ? 'show' : ''}`}
+                style={{
+                    display: `${this.props.showModal ? 'block' : 'none'}`,
+                }}
+                id="BettingModal" tabIndex="-1" role="dialog" aria-labelledby="ModalLabel" aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <div className="camera-box">
+                                <img alt="" src="/img/yellow-logo.svg" />
+                                <h5 className="modal-title" id="ModalLabel">Ladies & Gentlemen, Place Your Bets!</h5>
+                            </div>
+                        </div>
+                        <div className="modal-body">
+                            <p className="text-center">
+                                In increments of 100, place your bet below. Good luck, Gamer!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
